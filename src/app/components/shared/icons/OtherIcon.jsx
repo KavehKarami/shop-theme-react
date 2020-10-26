d28278c9a638a4a69ceb5f78be455c062524b963
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-26 18:00:11 +0330

    fix react jsx rules

--- src/app/components/shared/icons/OtherIcon.jsx
+++ src/app/components/shared/icons/OtherIcon.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const OtherIcon = () => {
   return (
-    <svg id="otherIcon" width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-three-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fill="none" stroke="#000" fill-rule="evenodd" d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+    <svg id="otherIcon" width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-three-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill="none" stroke="#000" fillRule="evenodd" d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
     </svg>
   );
 }
--- src/app/components/shared/icons/PriceIcon.jsx
+++ src/app/components/shared/icons/PriceIcon.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PriceIcon = () => {
   return (
-    <svg id="priceIcon" width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fill="none" stroke="#000" fill-rule="evenodd" d="M15 4H1v8h14V4zM1 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H1z" />
+    <svg id="priceIcon" width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-cash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill="none" stroke="#000" fillRule="evenodd" d="M15 4H1v8h14V4zM1 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H1z" />
       <path fill="none" stroke="#000" d="M13 4a2 2 0 0 0 2 2V4h-2zM3 4a2 2 0 0 1-2 2V4h2zm10 8a2 2 0 0 1 2-2v2h-2zM3 12a2 2 0 0 0-2-2v2h2zm7-4a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
     </svg>
   );
