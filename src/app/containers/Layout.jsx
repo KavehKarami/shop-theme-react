14df20d31bab4b2f50964f70813f6da194ef7799
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-26 18:07:37 +0330

    update Layout

--- src/app/containers/Layout.jsx
+++ src/app/containers/Layout.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import Header from "../components/layout/Header"
+// import Header from "../components/layout/Header"
 import Navbar from '../components/layout/Navbar';
-// import Sidebar from "../components/layout/Sidebar"
 import Footer from '../components/layout/Sidebar';
 
 const Layout = (props) => {
   return (
     <React.Fragment>
       <Navbar />
-      <Header />
-      {/* <Sidebar /> */}
+      {/* <Header /> */}
       {props.children}
       <Footer />
     </React.Fragment>
