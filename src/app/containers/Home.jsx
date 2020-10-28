d671393d0410ae3595d452845e7c442d43f27979
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-28 17:08:53 +0330

    use type.js in Home component

--- src/app/containers/Home.jsx
+++ src/app/containers/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Typed from 'react-typed';
 import ShapeDivider1 from '../components/home/ShapeDivider1';
 
 const Home = () => {
@@ -7,8 +8,22 @@ const Home = () => {
     <div className="homeContainer">
       <div className="position-relative homeIntro">
         <div className="py-3 px-5">سایت ما</div>
-        <h1 className="title text-center p-md-4">بهترین سایت ها</h1>
-        <h2 className="description text-center p-2 p-md-4">خیالت راحت باشه ما هواتو داریم</h2>
+        <h1 className="title text-center p-md-4">
+          <Typed
+            strings={['بهترین سایت ها']}
+            typeSpeed={60}
+            showCursor={false}
+          // stopped={false}
+          />
+        </h1>
+        <h2 className="description text-center p-2 p-md-4">
+          <Typed
+            strings={['خیالت راحت باشه ما هواتو داریم', 'رویاهات با ما تحقق پیدا میکنن']}
+            typeSpeed={60}
+            backSpeed={30}
+            loop
+          />
+        </h2>
         <div className="px-4 my-1 p-md-4 btn-box text-center">
           <Link to="price" className="btn btn-primary">سفارش دهید</Link>
         </div>
