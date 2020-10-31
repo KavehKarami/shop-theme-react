1231b267a0d3c206ab6c7b0cea28a040cbb0218a
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-31 13:45:12 +0330

    implement routes for mobile nav

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,7 +1,37 @@
+import React from "react";
 import Home from "./containers/Home";
 import About from "./containers/About";
+import Pricing from "./containers/Pricing";
+import HomeIcon from "./components/shared/icons/HomeIcon";
+import PriceIcon from "./components/shared/icons/PriceIcon";
+import AboutUsIcon from "./components/shared/icons/AboutUsIcon";
 
 export const routes = [
-  { path: "/", component: Home, exact: true, label: "Home" },
-  { path: "/about", component: About, exact: true, label: "About" },
+  { path: "/", component: Home, exact: true, label: "خانه" },
+  { path: "/pricing", component: Pricing, exact: true, label: "تعرفه ها" },
+  { path: "/about", component: About, exact: true, label: "درباره ما" },
+];
+
+export const mobileNavRouter = [
+  {
+    path: "/about",
+    component: About,
+    exact: true,
+    label: "درباره ما",
+    icon: <AboutUsIcon />,
+  },
+  {
+    path: "/pricing",
+    component: Pricing,
+    exact: true,
+    label: "تعرفه ها",
+    icon: <PriceIcon />,
+  },
+  {
+    path: "/",
+    component: Home,
+    exact: true,
+    label: "خانه",
+    icon: <HomeIcon />,
+  },
 ];
