37d4d51a226e403d55f9fcd8b94b01bc584bf528
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-31 13:10:50 +0330

    implement BtnWater component

---
+++ src/app/components/shared/buttons/BtnWater.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BtnWater = (props) => {
+  return (
+    <div className={props.parentClass || "d-flex justify-content-center"}>
+      <Link to={props.href} class="btn-water">
+        {props.title}
+      </Link>
+    </div>
+  );
+}
+
+export default BtnWater;
