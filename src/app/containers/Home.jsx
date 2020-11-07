aad2cbcc3b47474b27ede4ef11ae498e43baa4ef
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-07 16:48:05 +0330

    improve styles

--- src/app/containers/Home.jsx
+++ src/app/containers/Home.jsx
@@ -8,35 +8,37 @@ const Home = () => {
     <div className="homeContainer">
       <HomeIntro />
 
-      <div className="container-fluid">
+
+      <section className="container-fluid">
+        <h1 className="text-dark text-center card-title">برخی از ویژگی های ما</h1>
         <div className="row mb-3">
           <div className="col-12 col-md-6">
             <div className="row flex-md-column flex-lg-row">
-              <div className="col-12 col-lg-6 card-box">
+              <article className="col-12 col-lg-6 card-box">
                 <FeatureCard title="لورم ایپسوم" imgURL={require('../../assets/img/code.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد." />
+              </article>
 
-              </div>
-              <div className="col-12 col-lg-6 card-box">
+              <article className="col-12 col-lg-6 card-box">
                 <FeatureCard title="لورم ایپسوم" imgURL={require('../../assets/img/design.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد." />
+              </article>
 
-              </div>
             </div>
           </div>
 
           <div className="col-12 col-md-6">
             <div className="row flex-md-column flex-lg-row">
-              <div className="col-12 col-lg-6 card-box">
+              <article className="col-12 col-lg-6 card-box">
                 <FeatureCard title="لورم ایپسوم" imgURL={require('../../assets/img/rocket.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد." />
+              </article>
 
-              </div>
-              <div className="col-12 col-lg-6 card-box">
+              <article className="col-12 col-lg-6 card-box">
                 <FeatureCard title="لورم ایپسوم" imgURL={require('../../assets/img/24-hours.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد." />
+              </article>
 
-              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
 
       <div className="text-dark">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, impedit aperiam sint minus totam et, illo rem doloremque optio, quisquam labore ratione voluptatem saepe reiciendis exercitationem corrupti voluptatibus nobis? Nihil.
