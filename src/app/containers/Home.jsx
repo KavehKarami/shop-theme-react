77d5f83be49f6f913d7de2b24d43f73fca643c9a
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-10 19:26:02 +0330

    refactor Home Container

--- src/app/containers/Home.jsx
+++ src/app/containers/Home.jsx
@@ -1,76 +1,14 @@
 import React from 'react';
+import FeaturesSection from '../components/home/FeaturesSection';
 import HomeIntro from '../components/home/HomeIntro';
-import FeatureCard from '../components/shared/cards/FeatureCard';
-import WorkSampleCard from '../components/shared/cards/WorkSampleCard';
+import WorkSampleSection from '../components/home/WorkSampleSection';
 
 const Home = () => {
-
   return (
     <div className="homeContainer">
       <HomeIntro />
-
-
-      <section className="container-fluid">
-        <h1 className="text-dark text-center card-title">برخی از ویژگی های ما</h1>
-        <div className="row mb-3">
-          <div className="col-12 col-sm-6">
-            <div className="row flex-md-column flex-lg-row">
-              <article className="col-12 col-lg-6 card-box">
-                <FeatureCard title="لورم ایپسوم" imgURL={require('../../assets/img/code.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد." />
-              </article>
-
-              <article className="col-12 col-lg-6 card-box">
-                <FeatureCard title="لورم ایپسوم" imgURL={require('../../assets/img/design.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد." />
-              </article>
-
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-6">
-            <div className="row flex-md-column flex-lg-row">
-              <article className="col-12 col-lg-6 card-box">
-                <FeatureCard title="لورم ایپسوم" imgURL={require('../../assets/img/rocket.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد." />
-              </article>
-
-              <article className="col-12 col-lg-6 card-box">
-                <FeatureCard title="لورم ایپسوم" imgURL={require('../../assets/img/24-hours.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد." />
-              </article>
-
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="container-fluid">
-        <h1 className="text-dark text-center card-title">برخی از نمونه کار های ما</h1>
-        <div className="row my-5">
-          <div className="col-12 col-sm-6">
-            <div className="row flex-md-column flex-lg-row">
-              <article className="col-12 col-lg-6 card-box">
-                <WorkSampleCard titr="لندینگ پیج روبیکا" title="لورم ایپسوم" imgURL={require('../../assets/img/rubika.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است." />
-              </article>
-
-              <article className="col-12 col-lg-6 card-box">
-                <WorkSampleCard titr="لندینگ پیج روبیکا" title="لورم ایپسوم" imgURL={require('../../assets/img/rubika.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است." />
-              </article>
-
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-6">
-            <div className="row flex-md-column flex-lg-row">
-              <article className="col-12 col-lg-6 card-box">
-                <WorkSampleCard titr="لندینگ پیج روبیکا" title="لورم ایپسوم" imgURL={require('../../assets/img/rubika.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است." />
-              </article>
-
-              <article className="col-12 col-lg-6 card-box">
-                <WorkSampleCard titr="لندینگ پیج روبیکا" title="لورم ایپسوم" imgURL={require('../../assets/img/rubika.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است." />
-              </article>
-
-            </div>
-          </div>
-        </div>
-      </section>
+      <FeaturesSection />
+      <WorkSampleSection />
 
       <div className="text-dark">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, impedit aperiam sint minus totam et, illo rem doloremque optio, quisquam labore ratione voluptatem saepe reiciendis exercitationem corrupti voluptatibus nobis? Nihil.
