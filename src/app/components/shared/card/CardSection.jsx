a2de594061d4a64c247b52290125c410aa671f55
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-14 16:26:53 +0330

    trim CardSection

--- src/app/components/shared/card/CardSection.jsx
+++ src/app/components/shared/card/CardSection.jsx
@@ -6,22 +6,7 @@ const CardSection = (props) => {
       <h1 className="text-dark text-center card-title">{props.title}</h1>
       <div className="row mb-3">
 
-        {/* <div className="col-12 col-sm-6">
-          <div className="row flex-md-column flex-lg-row">
-            
-            <article className="col-12 col-lg-6 card-box">
-              <WorkSampleCard titr="لندینگ پیج روبیکا" title="لورم ایپسوم" imgURL={require('../../assets/img/rubika.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است." />
-            </article>
-
-            <article className="col-12 col-lg-6 card-box">
-              <WorkSampleCard titr="لندینگ پیج روبیکا" title="لورم ایپسوم" imgURL={require('../../assets/img/rubika.png')} description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است." />
-            </article>
-
-          </div>
-        </div> */}
-
         {props.children}
-
 
       </div>
     </section>
