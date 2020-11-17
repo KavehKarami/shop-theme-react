286d02e862c2226eab896c62cc813f2fecdb748e
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-17 16:07:56 +0330

    fix jsx rules

--- src/app/components/shared/inputs/DashedInput.jsx
+++ src/app/components/shared/inputs/DashedInput.jsx
@@ -6,15 +6,15 @@ const DashedInput = (props) => {
 
       <input type="email" placeholder={props.placeholder} required />
 
-      <div class="hover-border-right"></div>
-      <div class="hover-border-y"></div>
+      <div className="hover-border-right"></div>
+      <div className="hover-border-y"></div>
 
       <button>
-        <div class="arrow-box">
-          <div class="arrow arrow--frist">
+        <div className="arrow-box">
+          <div className="arrow arrow--frist">
             <img src="https://i.ibb.co/SyHKZF6/arrow-right.png" alt="" />
           </div>
-          <div class="arrow arrow--hover">
+          <div className="arrow arrow--hover">
             <img src="https://i.ibb.co/SyHKZF6/arrow-right.png" alt="" />
           </div>
         </div>
