b2fa69cae48b0cb7a88733d8b8624a262d58f633
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-17 16:22:49 +0330

    wip - footer

--- src/app/components/layout/Footer.jsx
+++ src/app/components/layout/Footer.jsx
@@ -1,40 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from '../../routes';
+import DashedInput from '../shared/inputs/DashedInput';
+import FollowUs from '../shared/sections/FollowUs';
+import ShapeDivider1 from '../shared/shape-dividers/ShapeDivider1';
 
 const Footer = () => {
   return (
-    <footer className="container-fluid text-dark mt-5 bg-dark">
-      <section className="row sub-to-news">
-        <div className="input-group">
-          <input type="text" />
-          <button className="text-dark"> عضویت </button>
+    <footer className="text-dark mt-5 pt-5 bg-dark">
+      <div className="container-fluid">
+        <section className="row sub-to-news">
+
+          <DashedInput className="mb-2" placeholder="example: asd@asd.com" />
+          <div className="mb-4">با عضویت در خبرنامه از آخرین تخفیفات و آفر های ما باخبر باشید.</div>
+
+
+          <FollowUs />
+
+        </section>
+
+        <div className="row pt-3">
+          <div className="col-12 my-4 mb-5">
+            <div className="footer-link-title">درباره ما</div>
+          </div>
         </div>
 
-        <div className="follow-us">
-          <i>tw</i>
-          <i>insta</i>
-          <i>git hub</i>
+        <div className="row">
+          <div className="col-6  my-4 mb-5">
+            <div className="footer-link-title">تماس با ما</div>
+          </div>
+          <div className="col-6  my-4 mb-5">
+            <div className="footer-link-title">دسترسی سریع</div>
+            <ul>
+              {routes.map((route, index) => (
+                <li key={index}>
+                  <Link to={route.path}>{route.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </section>
+      </div>
 
-      <div className="row mt-3">
-        <div className="col-4">
-          <div class="footer-link-title">درباره ما</div>
-        </div>
-        <div className="col-4">
-          <div class="footer-link-title">تماس با ما</div>
-        </div>
-        <div className="col-4">
-          <div class="footer-link-title">دسترسی سریع</div>
-          <ul>
-            {routes.map((route, index) => (
-              <li key={index}>
-                <Link to={route.path}>{route.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="position-relative w-100 d-md-none">
+        <ShapeDivider1 />
       </div>
     </footer>
   );
