cb43177858392db8cebde868099eacb3ab01bd24
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-17 18:05:00 +0330

    wip-footer (footer mobile complete)

--- src/app/components/layout/Footer.jsx
+++ src/app/components/layout/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from '../../routes';
+import MailIcon from '../shared/icons/MailIcon';
+import PhoneIcon from '../shared/icons/PhoneIcon';
 import DashedInput from '../shared/inputs/DashedInput';
 import FollowUs from '../shared/sections/FollowUs';
 import ShapeDivider1 from '../shared/shape-dividers/ShapeDivider1';
@@ -26,10 +28,29 @@ const Footer = () => {
         </div>
 
         <div className="row">
-          <div className="col-6  mt-3 mb-5">
+          <div className="col-12 col-md-4  mt-3 mb-5">
+
             <div className="footer-link-title">تماس با ما</div>
+
+            <div className="d-flex mb-3 align-items-center justify-content-start">
+              <PhoneIcon />
+
+              <a href="callto:+989186948289" className="mr-3 footer-contact-text">
+                +(98)918-694-8289
+              </a>
+            </div>
+
+            <div className="d-flex mb-3 align-items-center justify-content-start text-nowrap">
+              <MailIcon />
+
+              <a href="maillto:Kaveh.Karami.KK@gmail.com" className="mr-3 footer-contact-text">
+                Kaveh.Karami.KK@gmail.com
+              </a>
+            </div>
+
           </div>
-          <div className="col-6  mt-3 mb-5">
+
+          <div className="col-6 col-md-4 mt-3 mb-5 d-none d-md-block">
             <div className="footer-link-title">دسترسی سریع</div>
             <ul>
               {routes.map((route, index) => (
