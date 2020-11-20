b1a8761a93cea72daeb0cce10b1ca726f1193e51
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-20 14:35:01 +0330

    footer section implemented completely

--- src/app/components/layout/Footer.jsx
+++ src/app/components/layout/Footer.jsx
@@ -6,13 +6,18 @@ import PhoneIcon from '../shared/icons/PhoneIcon';
 import DashedInput from '../shared/inputs/DashedInput';
 import FollowUs from '../shared/sections/FollowUs';
 import ShapeDivider1 from '../shared/shape-dividers/ShapeDivider1';
+import ShapeDivider2 from '../shared/shape-dividers/ShapeDivider2';
 
 const Footer = () => {
   return (
-    <footer className="text-dark mt-5 pt-5 bg-dark">
-      <div className="container-fluid">
+    <footer className="text-dark mt-5 bg-dark">
+      <div className="position-relative w-100 mb-3">
+        <ShapeDivider2 />
+      </div>
 
-        <section className="row sub-to-news mb-md-5">
+      <div className="container-fluid footer-main">
+
+        <section className="row sub-to-news mb-md-5 flex-column flex-md-row-reverse">
           <div className="position-relative">
             <DashedInput className="mb-2 mb-md-0 mr-md-5" placeholder="example: asd@asd.com" />
             <div className="mb-4 mb-md-0 mt-md-3 pr-md-5  input-caption">با عضویت در خبرنامه از آخرین تخفیفات و آفر های ما باخبر باشید.</div>
