dae0ba35ab5e6430988763acabd4e389c1e850ae
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-23 17:00:47 +0330

    implement Pricing Container

--- src/app/containers/Pricing.jsx
+++ src/app/containers/Pricing.jsx
@@ -2,7 +2,102 @@ import React from 'react';
 
 const Pricing = () => {
   return (
-    <div>Pricing Works!</div>
+    <section class="container pricing-section d-flex justify-content-center align-items-center">
+
+      <article class="pricing-card">
+
+        <div class="iconBx">
+          <img src="https://i.ibb.co/YXTWzVQ/bronze.png" alt="" />
+        </div>
+
+        <div class="card-title">Lorem Ipusem</div>
+
+        <ul class="pricing-feature">
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+
+        </ul>
+
+        <div class="btnBx">
+          <button>
+            Order Now!
+      </button>
+        </div>
+
+      </article>
+
+      <article class="pricing-card gold-product">
+
+        <div class="iconBx">
+          <img src="https://i.ibb.co/5v5VtTh/gold.png" alt="" />
+        </div>
+
+        <div class="card-title">Lorem Ipusem</div>
+
+        <ul class="pricing-feature">
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+
+        </ul>
+
+        <div class="btnBx">
+          <button>
+            Order Now!
+      </button>
+        </div>
+
+      </article>
+
+      <article class="pricing-card">
+
+        <div class="iconBx">
+          <img src="https://i.ibb.co/Ht9p6cb/silver.png" alt="" />
+        </div>
+
+        <div class="card-title">Lorem Ipusem</div>
+
+        <ul class="pricing-feature">
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+          <li class="doesnt-have">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, illum!</li>
+
+        </ul>
+
+        <div class="btnBx">
+          <button>
+            Order Now!
+      </button>
+        </div>
+
+      </article>
+
+    </section>
   );
 }
 
