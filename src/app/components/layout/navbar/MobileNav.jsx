a2483ce6bcd646d63cbfc21a4da1e23fa7dd258a
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-27 14:58:38 +0330

    add monile-menue

--- src/app/components/layout/navbar/MobileNav.jsx
+++ src/app/components/layout/navbar/MobileNav.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { mobileNavRouter } from '../../../routes';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { mobileNavRouter, routes } from '../../../routes';
 import OtherIcon from '../../shared/icons/OtherIcon';
 import CustomLink from '../../utils/CustomLink';
 
 const MobileNav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const location = useLocation();
 
   useEffect(() => {
@@ -31,26 +34,50 @@ const MobileNav = () => {
     })
   }
 
+  const handleMenu = (e) => {
+    const condition = (!!e.target.id && !!((e.target.id === "otherIcon" || "otherBtn" || "mobileMenu")));
+
+    if (!condition && isMenuOpen) {
+      setIsMenuOpen(false)
+    }
+  }
+
+  window.addEventListener('click', handleMenu);
+
+  const handleMobileMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   document.addEventListener("DOMContentLoaded", handleTabBar);
   document.getElementsByTagName("BODY")[0].onresize = function () { handleTabBar() };
 
   return (
-    <nav className="tab-bar-box d-md-none">
-      <div className="tab-bar">
-        <div className="tab-indicator">
-          <div className="tab-indicator-left"></div>
-          <div className="tab-indicator-right"></div>
-        </div>
-        <div className="tab-others">
-          <div className="tab-item-inner d-flex flex-column">
-            <OtherIcon />
-            سایر
+    <React.Fragment>
+      <nav className="tab-bar-box d-md-none">
+        <div className="tab-bar">
+          <div className="tab-indicator">
+            <div className="tab-indicator-left"></div>
+            <div className="tab-indicator-right"></div>
           </div>
-        </div>
+          <div className="tab-others" onClick={handleMobileMenu}>
+            <div id="otherBtn" className="tab-item-inner d-flex flex-column">
+              <OtherIcon />
+              سایر
+            </div>
+          </div>
 
-        {mobileNavRouter.map((route, index) => <CustomLink key={index} path={route.path} exact={route.exact} label={route.label} icon={route.icon} />)}
-      </div>
-    </nav>
+          {mobileNavRouter.map((route, index) => <CustomLink key={index} path={route.path} exact={route.exact} label={route.label} icon={route.icon} />)}
+        </div>
+      </nav>
+
+      <section className={isMenuOpen ? "mobile-menu open-menu" : "mobile-menu"} id="mobileMenu">
+        <ul className="menu-box my-5 mx-3">
+          {routes.map((route, index) => (
+            <li className={!route.label ? "d-none" : undefined} key={index}><Link className={location.pathname === route.path ? "menu menu-active" : "menu"} to={route.path}>{route.label}</Link></li>
+          ))}
+        </ul>
+      </section>
+    </React.Fragment>
   );
 }
 
