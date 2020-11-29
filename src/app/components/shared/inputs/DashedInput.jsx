5c09d58eec93572aecfa0d5bb1a42af14aa3649f
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-11-29 18:21:01 +0330

    make DashedInput rtl

--- src/app/components/shared/inputs/DashedInput.jsx
+++ src/app/components/shared/inputs/DashedInput.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const DashedInput = (props) => {
   return (
-    <div className={`${props.className} dashed-input-gp input-group w-auto`}>
+    <div className={`${props.className} ${props.rtl ? "dashed-input-gp-rtl input-group w-auto" : "dashed-input-gp input-group w-auto"}`}>
 
-      <input type="email" placeholder={props.placeholder} required />
+      <input type={props.inputType || "email"} placeholder={props.placeholder} required />
 
       <div className="hover-border-right"></div>
       <div className="hover-border-y"></div>
@@ -12,10 +12,10 @@ const DashedInput = (props) => {
       <button>
         <div className="arrow-box">
           <div className="arrow arrow--frist">
-            <img src="https://i.ibb.co/SyHKZF6/arrow-right.png" alt="" />
+            {props.iconComponent ? props.icon : <img src={props.icon || "https://i.ibb.co/SyHKZF6/arrow-right.png"} alt="" />}
           </div>
           <div className="arrow arrow--hover">
-            <img src="https://i.ibb.co/SyHKZF6/arrow-right.png" alt="" />
+            {props.iconComponent ? props.icon : <img src={props.icon || "https://i.ibb.co/SyHKZF6/arrow-right.png"} alt="" />}
           </div>
         </div>
       </button>
